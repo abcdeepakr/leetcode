2ae1b90fc5e23a8230465b26628835d14e5d08db
Deepak Rawat <awsmyo1234@gmail.com>
2020-09-30 16:37:00 +0000

    commented the code below

--- thirdMax.py
+++ thirdMax.py
@@ -15,17 +15,17 @@ def secondMax(alist):
 # alist = [1,2,3,4,3,4,5,6,5,6,12,7,15,67,78,76] 
 list1 = [2, 2, 3, 1]
 
-mx=max(list1[0],list1[1]) 
-secondmax=min(list1[0],list1[1]) 
-n =len(list1) 
-for i in range(2,n): 
-	if list1[i]>mx: 
-		secondmax=mx 
-		mx=list1[i] 
-	elif list1[i]>secondmax and mx != list1[i]: 
-		secondmax=list1[i] 
-	else: 
-		if secondmax == mx: 
-			secondmax = list1[i] 
+# mx=max(list1[0],list1[1]) 
+# secondmax=min(list1[0],list1[1]) 
+# n =len(list1) 
+# for i in range(2,n): 
+# 	if list1[i]>mx: 
+# 		secondmax=mx 
+# 		mx=list1[i] 
+# 	elif list1[i]>secondmax and mx != list1[i]: 
+# 		secondmax=list1[i] 
+# 	else: 
+# 		if secondmax == mx: 
+# 			secondmax = list1[i] 
 
 print("Second highest number is : ",str(secondmax))
